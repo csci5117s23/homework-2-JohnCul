6fdb6710cf8a83c6e383e0351793f4fc873b65fe
John Cullom / cullo006 <johnccullom@gmail.com>
2023-04-10 13:46:14 -0500

    netlify testing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,26 +8,25 @@ import { ClerkProvider } from '@clerk/nextjs'
 export default function App({ Component, pageProps }) {
   return <>
   <ClerkProvider {...pageProps} >
+    <div className={styles.topmenu}>
+    <div className="topmenu pure-g" id="menu">
+        <div className="pure-u-1-4">
+          <h2>Todo List</h2>
+        </div>
+        <div className="pure-u-1-4">
+            <Link href="todos">Todos
+            </Link>
+        </div>
+        <div className="pure-u-1-4"><Link href="done">Done
+            </Link>
+          </div>
+        <div className="pure-u-1-4"><p>Categories</p>
+        </div>
+    </div>
+    </div>
 
     <Component {...pageProps} />
 
   </ClerkProvider>
-  <div className={styles.topmenu}>
-  <div className="topmenu pure-g" id="menu">
-      <div className="pure-u-1-4">
-        <h2>Todo List</h2>
-      </div>
-      <div className="pure-u-1-4">
-          <Link href="todos">Todos
-          </Link>
-      </div>
-      <div className="pure-u-1-4"><Link href="done">Done
-          </Link>
-        </div>
-      <div className="pure-u-1-4"><p>Categories</p>
-      </div>
-  </div>
-  </div>
-  <Component {...pageProps} />
   </>
 }
